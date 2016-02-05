ee989cde7458105b8b7ffdf31b8a2c76b9228bc9
Ben Youngblood <bx.youngblood@gmail.com>
2016-02-04 18:30:15 -0700

    easy support for api keys (BC break)

--- client.go
+++ client.go
@@ -16,10 +16,24 @@ var APIBaseURL = &url.URL{
 // DefaultClient is the default API client.
 var DefaultClient = &Client{}
 
+// APIKey is an EVE Online API key (key id + vcode)
+type APIKey struct {
+	KeyID string
+	VCode string
+}
+
+// NewAPIKey returns an APIKey with the corresponding values set
+func NewAPIKey(keyID, vCode string) APIKey {
+	return APIKey{
+		KeyID: keyID,
+		VCode: vCode,
+	}
+}
+
 // Call issues a GET request to the specified endpoint, which make be either a
 // relative or absolute URL.
-func Call(endpoint string, v interface{}) (*Metadata, error) {
-	return DefaultClient.Call(endpoint, v)
+func Call(endpoint string, apiKey *APIKey, v interface{}) (*Metadata, error) {
+	return DefaultClient.Call(endpoint, apiKey, v)
 }
 
 // Client represents an EVE API client. The zero value for Client is a full
@@ -40,12 +54,19 @@ func (c *Client) httpClient() *http.Client {
 
 // Call issues a GET request to the specified endpoint, which make be either a
 // relative or absolute URL.
-func (c *Client) Call(endpoint string, v interface{}) (*Metadata, error) {
+func (c *Client) Call(endpoint string, apiKey *APIKey, v interface{}) (*Metadata, error) {
 	parsedURL, err := url.Parse(endpoint)
 	if err != nil {
 		return nil, err
 	}
 	resolvedURL := APIBaseURL.ResolveReference(parsedURL)
+
+	if apiKey != nil {
+		query := resolvedURL.Query()
+		query.Set("keyID", apiKey.KeyID)
+		query.Set("vCode", apiKey.VCode)
+		resolvedURL.RawQuery = query.Encode()
+	}
 
 	res, err := c.httpClient().Get(resolvedURL.String())
 	if err != nil {
